1369a55eb203f3f5078afcb2a3add8dcbe2f5bec
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-09-15 03:56:58 +0300

    refactor home page, added delete function

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
 import styled from 'styled-components'
 
@@ -33,14 +32,39 @@ const Home = () => {
       })
   }, [])
   console.log(notes)
+
+  const deleteNote = (_id) => {
+    axios
+      .delete(`http://localhost:5500/notes/${_id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: {
+          id: _id,
+        },
+      })
+      .then((resp) => {
+        console.log(resp.message)
+      })
+      .catch((err) => console.error)
+    const newNotes = notes.filter((el) => el._id !== _id)
+    setNotes(newNotes)
+  }
+
   return (
     <Container>
       {notes.map((note) => (
-        <NoteCard>
-          <h1>{note.title}</h1>
-          <p>{note.content}</p>
-          <h4>{note.topic}</h4>
-        </NoteCard>
+        <div>
+          <NoteCard id={note._id}>
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+            <h4>{note.topic}</h4>
+          </NoteCard>
+          <div>
+            <button>Edit</button> |
+            <button onClick={() => deleteNote(note._id)}>Delete</button>
+          </div>
+        </div>
       ))}
     </Container>
   )
